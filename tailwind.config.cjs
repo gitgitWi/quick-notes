aad02b326b032cf0f75701eaea68f7c5fc59cd61
gitgitWi <gitgitWi@gmail.com>
2022-12-30 01:03:50 +0900

    feat(scss) add font-family Pretendard, add tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,20 @@
-module.exports = {
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/**
+ * @type {import('tailwindcss').Config}
+ */
+const configs = {
   content: ['./src/**/*.{astro,html,md,mdx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard Variable', 'Pretendard'].concat(
+          defaultTheme.fontFamily.sans
+        ),
+      },
+    },
   },
   plugins: [],
 };
+
+module.exports = configs;
