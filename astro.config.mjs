3b10f27791d07e7e75a805bd1426b1dfe80375e4
gitgitWi <gitgitWi@gmail.com>
2022-12-30 00:22:51 +0900

    feat(tailwindcss) install deps, add astro-tailwind config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), preact()],
+  integrations: [mdx(), tailwind(), preact()],
 });
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,md,mdx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
