23eaf47bb2b1dac66be9d0e382a5e5952f8d24ed
gitgitWi <gitgitWi@gmail.com>
2022-12-30 21:48:52 +0900

    fix(astro) set base path, remove base path from site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [mdx(), tailwind(), preact()],
-  site: 'https://gitgitWi.github.io/quick-notes',
+  site: 'https://gitgitWi.github.io',
+  base: '/quick-notes',
 });
