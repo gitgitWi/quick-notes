bb6a067883ddc059584fed84cfb89d4fc2419433
gitgitWi <gitgitWi@gmail.com>
2022-12-31 19:45:55 +0900

    feat(card/tags) add styles

--- src/features/cards/card-tags.tsx
+++ src/features/cards/card-tags.tsx
@@ -8,9 +8,14 @@ type CardTagsProps = PropsWithClassNames<{
 
 export function CardTags({ tags, classNames = [] }: CardTagsProps) {
   return (
-    <div class={[''].concat(classNames).join(' ')}>
+    <div
+      class={['flex flex-row justify-start italic']
+        .concat(classNames)
+        .join(' ')}
+    >
       {tags.map((tag) => (
-        <p class={[].join(' ')}>{tag}</p>
+        // TODO: posts-of-tag link
+        <p class={['mr-1 hover:underline'].join(' ')}>#{tag}</p>
       ))}
     </div>
   );
