daf7344c72920eb065e79e082a5839c393866252
gitgitWi <gitgitWi@gmail.com>
2023-01-01 00:40:22 +0900

    feat(list/ul) add comps. to default mdx comps.

--- src/features/mdx/constants.ts
+++ src/features/mdx/constants.ts
@@ -1,7 +1,10 @@
 import { H2, H3, H4 } from '@features/ui/headings';
+import { DefaultUl, DefaultUlLi } from '@features/ui/list';
 
 export const DEFAULT_CUSTOM_COMPONENTS = Object.freeze({
   h2: H2,
   h3: H3,
   h4: H4,
+  ul: DefaultUl,
+  li: DefaultUlLi,
 });
