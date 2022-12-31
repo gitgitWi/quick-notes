0e69a4eb60dd81471c713f4ff38c34e9c9cede80
gitgitWi <gitgitWi@gmail.com>
2023-01-01 00:21:45 +0900

    feat(headings) impl. h2, h3, h4 headings

---
+++ src/features/ui/headings/h4.tsx
@@ -0,0 +1,19 @@
+/** @jsxImportSource preact */
+
+import type { PropsWithClassNames, PropsWithPreactChildren } from '@/types';
+
+type H4Props = PropsWithClassNames<PropsWithPreactChildren>;
+
+export function H4({ classNames = [], children }: H4Props) {
+  return (
+    <h4
+      class={[
+        'max-w-[90%] my-2 ml-2 text-xl text-indigo-200/90 font-bold underline whitespace-normal break-words',
+      ]
+        .concat(classNames)
+        .join(' ')}
+    >
+      {children}
+    </h4>
+  );
+}
---
+++ src/features/ui/headings/index.ts
@@ -0,0 +1,3 @@
+export { H2 } from './h2';
+export { H3 } from './h3';
+export { H4 } from './h4';
