1d62e29acf7990cec5dee8ba57c850c453e62e9c
gitgitWi <gitgitWi@gmail.com>
2023-01-01 00:21:10 +0900

    feat(types/component) declare type PropsWithPreactChildren

--- src/types/components.ts
+++ src/types/components.ts
@@ -1,1 +1,6 @@
-export type PropsWithClassNames<T> = T & { classNames?: string[] };
+import type { ComponentChildren } from 'preact';
+
+export type PropsWithClassNames<T = {}> = T & { classNames?: string[] };
+export type PropsWithPreactChildren<T = {}> = T & {
+  children: ComponentChildren;
+};
