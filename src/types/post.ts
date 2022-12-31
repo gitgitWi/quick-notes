0e3f9518e5331a3cd32edc879110b52e46403f98
gitgitWi <gitgitWi@gmail.com>
2022-12-31 14:43:59 +0900

    feat(types/post) declare post frontmatter type

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,9 +1,12 @@
-export type Frontmatter = {
-  thumbnailUrl?: string;
-} & Frontmatter$Core;
+export type Frontmatter = Frontmatter$Core & Frontmatter$Post;
 
 export type Frontmatter$Core = {
   title: string;
   url: string;
   updatedAt: string;
+};
+
+export type Frontmatter$Post = {
+  description?: string;
+  thumbnailUrl?: string;
 };
