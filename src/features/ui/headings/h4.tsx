ba84c04ebf964f8e353d7ee5e781f48536e36029
gitgitWi <gitgitWi@gmail.com>
2023-01-04 22:00:22 +0900

    feat(headings/h4) add id

--- src/features/ui/headings/h4.tsx
+++ src/features/ui/headings/h4.tsx
@@ -2,11 +2,12 @@
 
 import type { PropsWithClassNames, PropsWithPreactChildren } from '@/types';
 
-type H4Props = PropsWithClassNames<PropsWithPreactChildren>;
+type H4Props = PropsWithClassNames<PropsWithPreactChildren<{ id: string }>>;
 
-export function H4({ classNames = [], children }: H4Props) {
+export function H4({ classNames = [], children, id }: H4Props) {
   return (
     <h4
+      id={id}
       class={[
         'my-2 ml-2 text-xl text-indigo-200/90 font-bold underline whitespace-normal break-words',
       ]
